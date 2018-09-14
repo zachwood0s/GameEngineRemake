8c26c76a3884c5df300031fa097d90c4b820d37d
Zach Woods <zach@hayzak.com>
2018-09-14 11:06:56 -0500

    Added a UIButton Component

---
+++ EngineCore/Components/UI/UIButtonComponent.cs
@@ -0,0 +1,28 @@
+﻿using ECS.Attributes;
+using ECS.Components;
+using ECS.Entities;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineCore.Components.UI
+{
+    [Component]
+    public class UIButtonComponent: ICopyableComponent
+    {
+        public Action<Entity> ButtonAction { get; set; }
+        public Rectangle ButtonBounds { get; set; }
+
+        public IComponent Copy()
+        {
+            return new UIButtonComponent()
+            {
+                ButtonAction = ButtonAction,
+                ButtonBounds = ButtonBounds
+            };
+        }
+    }
+}
